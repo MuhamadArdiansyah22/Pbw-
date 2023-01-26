924f84ddfc6de8a9c1d19aa246dd967fe46a035c
MuhamadArdiansyah22 <114278561+MuhamadArdiansyah22@users.noreply.github.com>
2023-01-26 19:00:46 +0700

    index.js pertemuan awal javascript

---
+++ index.js
@@ -0,0 +1,62 @@
+let a = 1;
+let b = '1';
+
+switch (true){
+    case a==b:
+    console.log('Ya');
+    break;
+    case a !==b:
+        console.log('No');
+        break;
+}
+
+switch (a){
+    case 1:
+    console.log('Ya');
+    break;
+    case 2:
+        console.log('No');
+        break;
+        default:
+            console.log('default')
+}
+
+// console.log(typeof a)
+
+// if (a instanceof Number){
+//     console.log('a is a number')
+// }
+
+// jika ingin mengecek value gunakan sama dengan === jangan ==
+
+// if (a > b){ 
+//     console.log('nilai salah')
+// } else {
+//     console.log('nilai benar')
+// }
+
+// if (a == b){ // === artinya nilai nya identik dengan yang dikanan 
+//     console.log('sama sama intiger')
+// } else {
+//     console.log('nilai berbeda')
+// }
+
+// console.log('hello word')
+
+// let a 
+// a = 10
+// const b = 20; // harus di isi awal 
+// // console.log(a++) // ga ngefek apa apa
+// // console.log(++a) // ++ diawal nambah 1
+
+// a *= 2;
+// // b *= 2; // Tidak bisa di ubah untuk const
+// console.log(`Variabel A bernilai ${a}`) // Template Literal
+
+
+
+
+
+
+// // console.log(10 % 3)
+// // console.log(10 **10)
